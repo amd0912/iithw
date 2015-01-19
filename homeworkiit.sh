193d5ec4cbe424e7ef86f6983e819ac4d3ebb465
amd0912 <amd_0912@yahoo.ro>
2015-01-19 16:08:12 +0200

    Create homeworkiit.sh

---
+++ homeworkiit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+function f{
+if [ -n "$1"]
+        then cd "$1"
+fi
+ls -R | egrep ":$|*.jpg$|*.jpeg$" | \
+sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/ /' -e 's/-/|/'
+
+if [ 'ls -F -1 | grep "/" | wc -l' = 0 ]
+      then echo "no subdir"
+fi
+ls -R -l | egrep '*.jpg$|*.jpeg$' | awk '{ x+= $5 } END { print "total size " x}'
+exit
+}
+if [ -z "$1" ]
+    then f
+    else f $1
+fi
