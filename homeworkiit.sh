fbcf695f1cb0956cff85e1bce7394ba0274c2b2f
amd0912 <amd_0912@yahoo.ro>
2015-01-20 09:20:43 +0200

    Update homeworkiit.sh

--- homeworkiit.sh
+++ homeworkiit.sh
@@ -3,8 +3,7 @@ function f{
 if [ -n "$1"]
         then cd "$1"
 fi
-ls -R | egrep ":$|*.jpg$|*.jpeg$" | \
-sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/ /' -e 's/-/|/'
+ls -R | egrep ":$|*.jpg$|*.jpeg$"
 
 if [ 'ls -F -1 | grep "/" | wc -l' = 0 ]
       then echo "no subdir"
